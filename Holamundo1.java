8e204c88d56cc20c8c0fcf1d2ab95c9de1515333
DrakoSk <113952438+DrakoSk@users.noreply.github.com>
2022-10-17 19:05:28 -0500

    Add files via upload

---
+++ Holamundo1.java
@@ -0,0 +1,30 @@
+
+package holamundo1;
+
+import javax.swing.JOptionPane;
+
+
+public class Holamundo1 {
+
+    
+    public static void main(String[] args) {
+        // 4 Operaciones que muestren al mismo tiempo los resultados
+        String a = JOptionPane.showInputDialog(null,"Ingrese el primer numero:");
+        String b = JOptionPane.showInputDialog(null, "Ingrese el segundo numero:");
+        double n1 = Double.parseDouble(a);
+        double n2 = Double.parseDouble(b);
+        double  suma = n1 + n2;
+        double resta = n1 - n2;
+        double mult = n1 * n2;
+        double div = n1 / n2;
+        JOptionPane.showMessageDialog(null, "Los resultados fueron: \n "
+                + "La suma fue: "+ suma +"\n"
+                + "La resta fue: "+ resta +"\n"
+                + "La miltiplicacion fue: " +mult+ "\n"
+                + "La division fue: " +div);
+                
+   
+  
+    }
+    
+}
